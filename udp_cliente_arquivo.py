2979c83dd7aabe1642a5c1ef0e05328df64c0259
Segundo Vale <twdsegundovale@gmail.com>
2024-11-12 11:22:40 -0300

    Criptografia gpg4win

--- udp_cliente_arquivo.py
+++ udp_cliente_arquivo.py
@@ -1,10 +1,19 @@
-from scapy.all import *
-import time
-
 import socket
 import time
+import subprocess
+
+def encrypt_file_with_public_key(filename, recipient):
+    """Criptografa o arquivo usando GPG e a chave pública do destinatário."""
+    try:
+        subprocess.run(['gpg', '--output', f"{filename}.gpg", '--encrypt', '--recipient', recipient, filename], check=True)
+        print(f"Arquivo '{filename}' criptografado com sucesso como '{filename}.gpg'.")
+    except subprocess.CalledProcessError:
+        print("Erro ao criptografar o arquivo.")
+        return False
+    return True
 
 def send_file_via_udp(filename, host='127.0.0.1', port=9999):
+    """Envia o arquivo criptografado via UDP."""
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     with open(filename, 'rb') as f:
@@ -22,4 +31,10 @@ def send_file_via_udp(filename, host='127.0.0.1', port=9999):
     sock.close()
 
 if __name__ == "__main__":
-    send_file_via_udp("arquivo.txt")
+    original_filename = "arquivo.txt"
+    encrypted_filename = f"{original_filename}.gpg"
+    recipient = "E7E7F0DCE8E6AAD877AFE08CC426E2A89F6A8474"  
+    
+    if encrypt_file_with_public_key(original_filename, recipient):
+        
+        send_file_via_udp(encrypted_filename)
--- udp_servidor_arquivo.py
+++ udp_servidor_arquivo.py
@@ -1,20 +1,27 @@
 import socket
+import subprocess
 
 def start_udp_server(host='127.0.0.1', port=9999):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((host, port))
     print(f'Servidor UDP em execução em {host}:{port}')
 
-    with open('arquivo.txt', 'wb') as f:
+    with open('arquivo.txt.gpg', 'wb') as f:
         while True:
             data, addr = sock.recvfrom(1024)  # Buffer de 1024 bytes
             if data == b'END':  # Sinaliza o fim da transferência
                 print("Transferência de arquivo concluída.")
                 break
             f.write(data)
-            print(f'Pacote recebido de {addr}: {data}')
+            print(f'Pacote recebido de {addr}')
 
     sock.close()
+
+    try:
+        subprocess.run(['gpg', '--output', 'arquivo_recebido.txt', '--decrypt', 'arquivo.txt.gpg'], check=True)
+        print("Arquivo descriptografado com sucesso como 'arquivo_recebido.txt'.")
+    except subprocess.CalledProcessError:
+        print("Erro ao descriptografar o arquivo.")
 
 if __name__ == "__main__":
     start_udp_server()
